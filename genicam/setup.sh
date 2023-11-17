6bf02136970aad2c189fa48eb1b63c73026a720c
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-11-17 08:46:19 +0100

    setup

--- genicam/setup.sh
+++ genicam/setup.sh
@@ -1,4 +1,23 @@
 echo Creating python virtual environment
 python3 -m venv venv
+
+echo Activating python virtual environment
 source venv/bin/activate
-pip install pycairo PyGObject
+
+echo Installing python dependencies
+pip install -r requirements.txt
+
+echo Updating apt
+sudo apt update
+
+echo Installing gstreamer
+sudo apt-get install gstreamer1.0-tools gstreamer1.0-alsa \
+     gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+     gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+     gstreamer1.0-libav
+
+echo Installing gstreamer development libraries
+sudo apt-get install libgstreamer1.0-dev \
+     libgstreamer-plugins-base1.0-dev \
+     libgstreamer-plugins-good1.0-dev \
+     libgstreamer-plugins-bad1.0-dev
