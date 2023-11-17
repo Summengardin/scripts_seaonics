d8c55da293e201d587edd9ede233393a171bdb2a
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-11-17 09:34:01 +0100

    non-nvidia decode

--- genicam/gst_viewer.sh
+++ genicam/gst_viewer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder enable-max-performance=0 ! nvvidconv ! autovideosink sync=0
+#gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder enable-max-performance=0 ! nvvidconv ! autovideosink sync=0
+
+gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 ! rtph264depay ! h264parse ! decodebin ! videoconvert ! autovideosink sync=0
