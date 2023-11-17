5fe94bbb6d4eec905e2249c2a43d0fcb09aa0658
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-11-17 14:58:04 +0100

    viewer 264

--- genicam/gst_viewer.sh
+++ genicam/gst_viewer.sh
@@ -2,4 +2,4 @@
 
 #gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder enable-max-performance=0 ! nvvidconv ! autovideosink sync=0
 
-gst-launch-1.0 rtspsrc location=rtsp://169.254.54.69:8554/test latency=0 protocols=udp-mcast+udp ! rtph265depay ! h265parse ! decodebin ! videoconvert ! autovideosink sync=0
+gst-launch-1.0 rtspsrc location=rtsp://169.254.54.69:8554/test latency=0 protocols=udp-mcast+udp ! rtph264depay ! h264parse ! decodebin ! videoconvert ! autovideosink sync=0
