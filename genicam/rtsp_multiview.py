54007664d607e19d5b33e6d090af285de74779ee
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-12-01 13:13:14 +0100

    Multiview frame

--- genicam/rtsp_multiview.py
+++ genicam/rtsp_multiview.py
@@ -121,19 +121,17 @@ def display_rtsp_frames_same_window(cam_grabbers, enable_logging=False):
 
             small_frame = cv2.resize(secondary_frame, (primary_frame.shape[1] // 4, primary_frame.shape[0] // 4))
 
-            # Calculate position for small frame
             x_offset = primary_frame.shape[1] - small_frame.shape[1]
             y_offset = primary_frame.shape[0] - small_frame.shape[0]
 
-            # Draw the frame around small_frame
-            frame_thickness = 10  # Thickness of the frame
-            border_color = (0, 255, 0)  # Frame color (green in this example)
-            cv2.rectangle(primary_frame, (x_offset, y_offset), (x_offset + small_frame.shape[1], y_offset + small_frame.shape[0]), border_color, frame_thickness)
-
-            # Place the small frame
             combined_frame = primary_frame.copy()
             combined_frame[y_offset:y_offset+small_frame.shape[0], x_offset:x_offset+small_frame.shape[1]] = small_frame
 
+            # Draw border around secondary frame
+            frame_thickness = 10
+            border_color = (0, 255, 0) 
+            cv2.rectangle(combined_frame, (x_offset, y_offset), (x_offset + small_frame.shape[1], y_offset + small_frame.shape[0]), border_color, frame_thickness)
+            
             cv2.imshow("Combined Frames", combined_frame)
             
 
