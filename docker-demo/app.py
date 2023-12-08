5fc4e370dbe3ac967f72b957131cd70fd1548eab
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-12-08 08:45:03 +0100

    Docker demo

---
+++ docker-demo/app.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = 3
+b = 5
+
+c = a + b
+
+print(f'{a} + {b} = {c}')
+
+A = np.array([1, 2, 3])
+B = np.array([4, 5, 6])
+
+C = A + B
+
+print(f'{A} + {B} = {C}')
+
+
+print("Program finished")
--- scripts/install_gstreamer.sh
+++ scripts/install_gstreamer.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y \
 libgstreamer1.0-dev \
 libgstreamer-plugins-base1.0-dev \
+libgstreamer-plugins-good1.0-dev \
 libgstreamer-plugins-bad1.0-dev \
 gstreamer1.0-plugins-base \
 gstreamer1.0-plugins-good \
