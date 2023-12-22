e6272a0ced88d7fe287b3ca1ad41057be607b2b2
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-12-22 10:58:54 +0100

    Start stream

--- scripts/start_stream.sh
+++ scripts/start_stream.sh
@@ -2,4 +2,33 @@
 
 cd /home/seaonics/Desktop/scripts_seaonics/genicam
 source venv/bin/activate
-python3 gentl_rtsp_server.py
+gnome-terminal -- python3 gentl_rtsp_server.py
+
+#!/bin/bash
+
+# Replace "/path/to/directory" with the path of the folder you want to open.
+FOLDER_PATH="/home/seaonics/Desktop/scripts_seaonics/genicam"
+
+# Replace "your_command_here" with the command you want to execute in the folder.
+COMMAND1= "source venv/bin/activate"
+COMMAND2= "python3 gentl_rtsp_server.py"
+
+# Check if the folder exists
+if [ -d "$FOLDER_PATH" ]; then
+    # Open the folder in terminal
+    cd "$FOLDER_PATH"
+    echo "Opened folder: $FOLDER_PATH"
+    
+    # Execute the command
+    echo "Executing command: $COMMAND1"    
+    eval $COMMAND2
+    
+    # Execute the command
+    echo "Executing command: $COMMAND2"    
+    eval $COMMAND2
+
+    # Keep the terminal open (on some systems, like macOS)
+    exec $SHELL
+else
+    echo "Folder does not exist: $FOLDER_PATH"
+fi
