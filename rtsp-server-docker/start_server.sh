59c38957a284b502401e89faff448275012e1daf
Martin-Jetson <summengardin@gmail.com/bin/python3.9>
2023-12-29 14:38:14 +0100

    start

--- rtsp-server-docker/start_server.sh
+++ rtsp-server-docker/start_server.sh
@@ -2,5 +2,5 @@
 
 #bash /home/seaonics/Desktop/scripts_seaonics/genicam/scripts/setmaxMTU.sh eth0 #POE port
 #bash /home/seaonics/Desktop/scripts_seaonics/genicam/scripts/setmaxMTU.sh docker0
-
+nmcli connection up eth-server
 gnome-terminal -- docker run -it --rm --net=host --runtime nvidia --gpus all -v /tmp/argus_socket:/tmp/argus_socket rtspserver
